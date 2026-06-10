c01cad37a1b44e62100dbd1aeba8022c96e6bb43
sean <114715930+654141454@users.noreply.github.com>
2026-06-10 16:45:55 +0800

    Delete week05-3.py

--- week05-3.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        counter = Counter(arr)
-        s= set()
-        for c in counter:
-            if counter[c] in s:
-                return False
-            s.add(counter[c])
-        return True
